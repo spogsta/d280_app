467587a6faf9fc979f25ee3da5fb0932652e60bf
spogsta~ <cheffjeff10@gmail.com>
2024-03-19 19:28:46 -0500

    aaa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorldComponent } from './world/world.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorldComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/world/world.component.ts
@@ -0,0 +1,14 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-world',
+  templateUrl: './world.component.html',
+  styleUrls: ['./world.component.scss']
+})
+export class WorldComponent {
+
+
+  
+}
+
